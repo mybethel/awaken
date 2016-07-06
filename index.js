93bbfe0bd67547c37845873f19bda3a15a7e2899
Albert Martin <amartin@treehouse.co>
2016-07-06 11:03:11 -0500

    feat(general): export awaken functionality as a class

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 const Heroku = require('heroku-client');
 const path = require('path');
 
-function Awaken(token, commandDirectory, app) {
-  this.token = token;
-  this.commands = path.resolve(process.cwd(), commandDirectory || './workers');
-  this.client = new Heroku({ token: this.token }).apps(app || process.env.HEROKU_APP);
-}
+class Awaken {
 
-Awaken.prototype.run = function(script, dynoSize, args) {
-  return new Promise((resolve, reject) => {
-    if (!script) return reject(new Error('A valid script is required to run'));
+  constructor(token, commandDirectory, app) {
+    this.token = token;
+    this.commands = path.resolve(process.cwd(), commandDirectory || './workers');
+    this.client = new Heroku({ token: this.token }).apps(app || process.env.HEROKU_APP);
+  }
 
-    if (!this.client || !process.env.HEROKU_APP) {
-      return this.local(script, args);
-    }
+  run(script, dynoSize, args) {
+    return new Promise((resolve, reject) => {
+      if (!script) return reject(new Error('A valid script is required to run'));
 
-    this.client.dynos().create({
-      command: `awaken ${path.join(this.commands, script)} ${args}`,
-      size: dynoSize || 'Free',
-    }, (err, result) => {
-      if (err) return reject(err);
-      resolve(result);
+      if (!this.client || !process.env.HEROKU_APP) {
+        return this.local(script, args);
+      }
+
+      this.client.dynos().create({
+        command: `awaken ${path.join(this.commands, script)} ${args}`,
+        size: dynoSize || 'Free',
+      }, (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
     });
-  });
-};
+  }
 
-Awaken.prototype.local = function(script, args) {
-  script = require(path.join(this.commands, script));
+  local(script, args) {
+    script = require(path.join(this.commands, script));
 
-  return new Promise((resolve, reject) => {
-    if (typeof script.run !== 'function')
-      return reject(new Error('Requested script is missing a run function'));
+    return new Promise((resolve, reject) => {
+      if (typeof script.run !== 'function')
+        return reject(new Error('Requested script is missing a run function'));
 
-    return script.run(args);
-  });
-};
+      return script.run(args);
+    });
+  }
+}
 
 module.exports = Awaken;
